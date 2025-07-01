cd2691b0a4cb9bca8d7cea24575fcabd95346e18
lachose-jB <jadjahouisso@gmail.com>
2025-07-01 07:25:41 +0200

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createHtmlPlugin({
+      inject: {
+        tags: [
+          {
+            tag: 'meta',
+            attrs: {
+              'http-equiv': 'Content-Security-Policy',
+              content: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; object-src 'none';"
+            },
+            injectTo: 'head'
+          },
+          {
+            tag: 'meta',
+            attrs: {
+              'http-equiv': 'X-Content-Type-Options',
+              content: 'nosniff'
+            },
+            injectTo: 'head'
+          },
+          {
+            tag: 'meta',
+            attrs: {
+              'http-equiv': 'X-Frame-Options',
+              content: 'DENY'
+            },
+            injectTo: 'head'
+          }
+        ]
+      }
+    })
+  ]
 })
